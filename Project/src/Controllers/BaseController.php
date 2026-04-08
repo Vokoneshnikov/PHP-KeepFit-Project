b91a7789ea0bce30e4b8580423aaad6af44e76d9
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-08 14:28:10 +0300

    just a commit

--- Project/src/Controllers/BaseController.php
+++ Project/src/Controllers/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Core\RequestInfo;
+
 abstract class BaseController {
     public function __construct(private RequestInfo $request) {}
     public abstract function index();
