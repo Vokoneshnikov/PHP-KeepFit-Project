3c8181c2e0152211ba83ed474b3329851bd80d72
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-10 16:45:05 +0300

    hw-07 for fullstack - added monolog, config, logger

---
+++ Project/Config/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Logger;
+use App\Core\Config;
+
+set_exception_handler(function ($exception) {
+    Logger::getInstance()->critical("Необработанное исключение: " . $exception->getMessage(), [
+        'file' => $exception->getFile(),
+        'line' => $exception->getLine(),
+        'type' => get_class($exception)
+    ]);
+
+    http_response_code(500);
+
+    $isDebug = $_ENV['APP_DEBUG'] ?? 'false';
+
+    if ($isDebug === 'true') {
+        echo "<h1>Debug Mode</h1>";
+        echo "<p><b>Error:</b> " . $exception->getMessage() . "</p>";
+        echo "<pre>" . $exception->getTraceAsString() . "</pre>";
+    } else {
+        $viewPath = __DIR__ . '/../views/errors/500.php';
+        if (file_exists($viewPath)) {
+            include $viewPath;
+        } else {
+            echo "<h1>500 Internal Server Error</h1><p>Упс! Что-то пошло не так.</p>";
+        }
+    }
+    exit;
+});
+
+//загрузка конфигурации
+Config::load(__DIR__ . '/../');
