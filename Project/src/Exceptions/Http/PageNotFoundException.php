2f9f6e63a0baeeed5b59d4596e5dd0b9f6764d28
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-11 01:02:32 +0300

    services and exceptions added

---
+++ Project/src/Exceptions/Http/PageNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions\Http;
+
+use App\Exceptions\AppException;
+
+class PageNotFoundException extends AppException {
+    protected int $statusCode = 404;
+
+    public function getHttpCode() : int {
+        return $this->statusCode;
+    }
+}
--- Project/src/Exceptions/InternalConfigException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Exceptions;
-
-class InternalConfigException extends \Exception {
-
-}
