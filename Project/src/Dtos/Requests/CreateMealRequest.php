789df780031861af1f105cb64c599b4892a13511
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-20 14:56:51 +0300

    meal repository done - enum, dtos, repository added

---
+++ Project/src/Dtos/Requests/CreateMealRequest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Dtos\Requests;
+
+use DateTimeImmutable;
+use App\Enums\MealType;
+
+class CreateMealRequest {
+    public function __construct(
+        public int $userId,
+        public int $foodId,
+        public int $amountGrams,
+        public MealType $mealType,
+        public DateTimeImmutable $consumedAt
+    ) {}
+
+}
--- Project/src/Repositories/Implementations/MealRepository.php
+++ Project/src/Repositories/Implementations/MealRepository.php
@@ -3,12 +3,166 @@ namespace App\Repositories\Implementations;
 
 use App\Repositories\interfaces\IMealRepository;
 
+use App\Dtos\Requests\CreateMealRequest;
+use App\Dtos\Requests\UpdateMealRequest;
+use App\Dtos\Responses\MealResponse;
+use App\Enums\MealType;
+use App\Core\Database;
+use App\Core\Logger;
+use PDOException;
+
 class MealRepository implements IMealRepository {
-    public function getById(int $id);
-    public function getAll();
+    private \PDO $pdo;
 
-    public function save();
+    public function __construct(?\PDO $pdo = null) {
+        $this->pdo = $pdo ?? Database::getConnection();
+    }
 
-    public function delete();
+    public function getById(int $id) : MealResponse {
+        try {
+        $stmt = $this->pdo->prepare("SELECT * FROM meals WHERE id = :id");
+        $stmt->execute([
+            "id" => $id,
+        ]);
+        $data = $stmt->fetch();
+        if (!$data) {
+            throw new \Exception("Прием пищи не найден"); 
+        }
+
+        $mealDto = new MealResponse (
+            id: $data['id'],
+            userId: $data['user_id'],
+            foodId: $data['food_id'],
+            mealType: MealType::from($data['meal_type']),
+            consumedAt: new \DateTimeImmutable($data['consumed_at']),
+            amountGrams : $data['amount_grams']
+        );
+
+        return $mealDto;
+        }
+        catch (PDOException $e) {
+            $msg = "Ошибка с запросом getById";
+            Logger::getInstance()->error($msg);
+            throw new \Exception($msg);
+
+        }
+    }
+    public function getAll() : array {
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM meals");
+            $stmt->execute();
+
+            $data = $stmt->fetchAll();
+
+            $meals = array_map(fn($row) =>  new MealResponse (
+                id: $row['id'],
+                userId: $row['user_id'],
+                foodId: $row['food_id'],
+                mealType: MealType::from($row['meal_type']),
+                consumedAt: new \DateTimeImmutable($data['consumed_at']),
+                amountGrams : $row['amount_grams'],
+            ), $data);
+
+            return $meals;
+        }
+        catch (PDOException $e){
+            $msg = "Ошибка с запросом getAll";
+            Logger::getInstance()->error($msg);
+            throw new \Exception($msg);
+        }
+
+    }
+
+    public function save(object $request){
+        return match(true) {
+            $request instanceof CreateMealRequest => $this->create($request),
+
+            $request instanceof UpdateMealRequest => $this->update($request),
+
+            default => throw new \InvalidArgumentException("...")
+        };
+    }
+
+    private function create(CreateMealRequest $request) : MealResponse{
+        try {
+            $stmt = $this->pdo->prepare("INSERT INTO meals (user_id, food_id, amount_grams, meal_type, consumed_at) VALUES (:user_id, :food_id, :amount_grams, :meal_type, :consumed_at) RETURNING id");
+            $stmt->execute([
+                "user_id" => $request->userId,
+                "food_id" => $request->foodId,
+                "amount_grams" => $request->amountGrams,
+                "meal_type" => $request->mealType->value,
+                "consumed_at" => $request->consumedAt->format('Y-m-d')
+            ]);
+
+            $result = $stmt->fetch();
+
+            return new MealResponse(
+                id: $result['id'],
+                userId: $request->userId,
+                foodId: $request->foodId,
+                amountGrams: $request->amountGrams,
+                mealType: $request->mealType,
+                consumedAt: $request->consumedAt,
+            );
+        }
+        catch (PDOException $e) {
+            $msg = "Ошибка с запросом create";
+            Logger::getInstance()->error($msg);
+            throw new \Exception($msg);
+        }
+    }
+
+    private function update(UpdateMealRequest $request) : MealResponse {
+        $updates = [];
+        $params = ['id' => $request->id];
+
+        if ($request->amountGrams != null) {
+            $updates[] = "amount_grams = :amount_grams";
+            $params['amount_grams'] = $request->amountGrams;
+        }
+        if ($request->mealType != null) {
+            $updates[] = "meal_type = :meal_type";
+            $params['meal_type'] = $request->mealType->value;
+        }
+
+        if (empty($updates)) {
+            return $this->getById($request->id);
+        }
+
+        try {
+            $stmt = $this->pdo->prepare("UPDATE meals SET " . implode(', ', $updates) . " WHERE id = :id");
+            $stmt->execute($params);
+
+            $meal = $this->getById($request->id);
+
+            if (!$meal) {
+                throw new \Exception("Прием пищи не найден.");
+            }
+            return $meal;
+        }
+        catch (PDOException $e) {
+            $msg = "Ошибка с запросом update";
+            Logger::getInstance()->error($msg);
+            throw new \Exception($msg);
+        }
+    }
+
+    public function delete(int $id) : bool{
+        try {
+            $stmt = $this->pdo->prepare("DELETE FROM meals WHERE id = :id");
+            $stmt->execute([
+                "id" => $id,
+            ]);
+            $deletedRows = $stmt->rowCount();
+
+            return ($deletedRows === 0) ? false : true;
+        }
+        catch (PDOException $e) {
+            $msg = "Ошибка с запросом delete";
+            Logger::getInstance()->error($msg);
+            throw new \Exception($msg);
+        }
+
+    }
 
 }
