7d1981d6a256c4b23515fad58db85d40568c5dbf
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-21 01:09:22 +0300

    example of working with repositories added

--- Project/Public/Index.php
+++ Project/Public/Index.php
@@ -1,5 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../config/bootstrap.php';
+use App\Repositories\Implementations\UserRepository;
+use App\Dtos\Responses\UserResponse;
 
-echo "Приложение работает!";
+$rep = new UserRepository();
+$users = $rep->getAll();
+
+foreach($users as $user) {
+    echo $user->name . "<br/>";
+}
+
+echo "Приложение работает! <br/>";
