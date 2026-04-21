641db2c7b304932b023c51186ccea346448ebb9f
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-21 03:53:02 +0300

    working on routing with attributes

--- Project/src/Core/Router.php
+++ Project/src/Core/Router.php
@@ -3,21 +3,46 @@
 namespace App\Core;
 
 use App\Controllers;
+use ReflectionClass;
 
 class Router {
+    private static $routes = [];
+    public function __construct() {}
 
-    public function __construct(private Routes $routes) {
+    public function register(array $controllers) {
+        
+    foreach($controllers as $controller) {
 
+        $reflector = new ReflectionClass($controller::class);
+
+        $methods = $reflector->getMethods();
+
+        foreach ($methods as $method) {
+
+            $attributes = $method->getAttributes(Route::class);
+
+            foreach($attributes as $attribure) {
+
+                $route = $attribure->getInstance();
+                $httpMethod = $route->httpMethod;
+                $path = $route->path;
+
+                $this->routes[strtoupper($httpMethod)][$path] = [
+                    'controller' => $controller,
+                    'method' => $method,
+                ];
+                }
+            }
+        }
     }
-
     public function run() {
         $requestInfo = new requestInfo();
 
-        $httpMethod = $requestInfo->method;
+        $httpMethod = strtoupper($requestInfo->method);
         $path = $requestInfo->path;
 
         try {
-            $route = $this->routes->getRoute($httpMethod, $path);
+            $route = $this->getRoute($path, $httpMethod);
             
             $controllerName = "App\\Controllers\\" . $route['controller'];
             $controller = new $controllerName();
@@ -33,7 +58,9 @@ class Router {
 
         }
     }
-
+    private function getRoute($path, $httpMethod) {
+        return $this->routes[$httpMethod][$path];
+    }
     private function sendNotFound(): void {
         header("HTTP/1.0 404 Not Found");
         echo "404 - Страница не найдена";
