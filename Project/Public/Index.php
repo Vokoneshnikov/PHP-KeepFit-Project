f92f444572d6672201277af5531a00d3ca5206b2
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-04-27 15:21:22 +0300

    minimal useful index.php

--- Project/Public/Index.php
+++ Project/Public/Index.php
@@ -1,14 +1,24 @@
 <?php
 
 require_once __DIR__ . '/../config/bootstrap.php';
-use App\Repositories\Implementations\UserRepository;
-use App\Dtos\Responses\UserResponse;
+// use App\Core\Route;
+// use App\Repositories\Implementations\UserRepository;
+// use App\Dtos\Responses\UserResponse;
+use App\Core\Router;
 
-$rep = new UserRepository();
-$users = $rep->getAll();
+// $rep = new UserRepository();
+// $users = $rep->getAll();
 
-foreach($users as $user) {
-    echo $user->name . "<br/>";
-}
-
-echo "Приложение работает! <br/>";
+// foreach($users as $user) {
+//     echo $user->name . "<br/>";
+// }
+$router = new Router();
+$router->register([
+    \App\Controllers\DiaryController::class,
+    \App\Controllers\FoodController::class,
+    \App\Controllers\FoodCustomController::class,
+    \App\Controllers\MealController::class,
+    \App\Controllers\ProfileController::class,
+    \App\Controllers\StatisticsController::class,
+    ]);
+$router->run();
