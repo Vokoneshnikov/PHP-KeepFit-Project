11e5a5bed1101abff8fb7f170eeb1f40998ad591
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-05-21 00:08:09 +0300

    custom food service/repository/controller added

--- Project/src/Repositories/Interfaces/IFoodRepository.php
+++ Project/src/Repositories/Interfaces/IFoodRepository.php
@@ -6,4 +6,5 @@ interface IFoodRepository extends IRepository
 {
     public function search(string $query): array;
     public function getRecentByUserId(int $userId): array;
+    public function getCustomByUserId(int $userId): array;
 }
