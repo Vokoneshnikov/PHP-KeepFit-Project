9024f19429d9082ed550944c148aacd4b033a21a
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-05-22 01:35:22 +0300

    changing pathes

--- Project/backend/Public/index.php
+++ Project/backend/Public/index.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../Config/bootstrap.php';
 
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 use App\Controllers\DiaryController;
 use App\Controllers\FoodController;
 use App\Controllers\FoodCustomController;
