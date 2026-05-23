dc00d02ea01f575430c5b064e97c302fc192df3f
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2026-05-24 01:36:24 +0300

    psr12

--- Project/backend/src/Repositories/Interfaces/IStatisticsRepository.php
+++ Project/backend/src/Repositories/Interfaces/IStatisticsRepository.php
@@ -5,7 +5,13 @@ namespace App\Repositories\Interfaces;
 interface IStatisticsRepository
 {
     public function saveDailyNorm(int $userId, int $calories, float $proteins, float $fats, float $carbs): void;
-    public function saveUserParameters(int $userId, float $weight, int $height, string $activityLevel, string $goal): void;
+    public function saveUserParameters(
+        int $userId,
+        float $weight,
+        int $height,
+        string $activityLevel,
+        string $goal
+    ): void;
     public function getWeeklyProgress(int $userId): array;
     public function getMonthlyProgress(int $userId): array;
     public function getAveragesAndNorms(int $userId, string $period = 'week'): array;
